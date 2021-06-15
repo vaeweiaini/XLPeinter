cdd0470aa9c97b1f877afe791745cda6bf5c80fb
ZhenyuNiu <vaeweiaini@outlook.com>
2021-06-14 21:47:00 -0400

    Initial Commit

---
+++ ChatNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatNow
+//
+//  Created by ZhenYu Niu on 2021-06-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
